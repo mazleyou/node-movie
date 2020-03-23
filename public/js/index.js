9a6922464b37276414bc50fa64faf58aeb8089f8
gjlim <gjlim@DESKTOP-CTUCPUK>
2020-03-23 18:40:40 +0900

    commit;

---
+++ public/js/index.js
@@ -0,0 +1,23 @@
+$(document).ready(function (){
+
+    $(document).on('change', '#fileUpload', function(e){
+        var formData = new FormData();
+        formData.append('mp4_file', $('#fileUpload')[0].files[0]);
+        
+        $.ajax({
+            type: 'POST',
+            enctype: 'multipart/form-data',
+            url: 'http://52.141.7.91:8200/emotion_detection/',
+            processData: false,
+            contentType: false,
+            data: formData,
+            success: function(res) {
+                console.log(res['fileName']);
+                $('#video > source').attr('src', 'http://52.141.7.91:8800/mp4/output.mp4');
+                $('#video').load();
+                document.getElementById("video").play();
+            }
+        });
+    });
+
+});
